b1b63e5153a573d168bdb0987a3102fa36aa293a
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2024-10-21 01:59:55 +0530

    Add files via upload

---
+++ dsa_java/arrays/dynamic.java
@@ -0,0 +1,12 @@
+package arrays;
+//staic- fixed size, compile time, stack
+//dynamic- not fixed, run time, heap
+public class dynamic{
+    public static void main (String args[]){
+        int [] arr = new int [3];
+        arr[0]=22;
+        arr[1]=23;
+        arr[2]=24;
+        System.out.println(arr[0]);
+    }
+}
