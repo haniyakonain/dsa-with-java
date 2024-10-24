45cc43e720d52fc2e0cd965e578ef735e72ec372
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2024-10-24 23:38:41 +0530

    Add files via upload

---
+++ dsa_java/leetcode/TwoSum.java
@@ -0,0 +1,28 @@
+package leetcode;
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> num_map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) { 
+            int complement = target - nums[i];
+            
+            if (num_map.containsKey(complement)) {
+                return new int[] {num_map.get(complement), i}; 
+            }
+            
+            num_map.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No solution found"); 
+    }
+}
+public class TwoSum {  // Note: filename should be TwoSum.java
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
