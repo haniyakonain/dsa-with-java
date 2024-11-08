6bdaf47a403ad3a3519110d527547e8e6a0c2850
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2024-11-08 23:59:50 +0530

    Update majority.java

--- dsa_java/hashmap/majority.java
+++ dsa_java/hashmap/majority.java
@@ -24,3 +24,15 @@ public class majority{
     majorityElement(nums);
     }
 }
+
+
+algotithm <br>
+    understand the question  <br>
+    Q) more than n/3 times  <br>
+    so if there is an array of integers like <br>
+    int nums[] = {1,3,2,5,1,3,1,5,1} so the value should be 9/3  <br>
+    which is 3 and the value which is greater than 3  is 5 and 5 is occuring 2 times  <br>
+    but hashmap doesnt take duplicates so 5 is counted as 1 <br>
+    hence the number of occurance is 1 <br>
+    similarly for {1,2} the n is 2 so 2/3 which is 0.6 <br>
+    and the value which is greater than 0.6 is 1,2 
