68a19e0de6ed6b18887f10b88e6581fb58f4500a
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-02-22 01:29:47 +0530

    Update dynamic.java

--- dsa_java/arrays/dynamic.java
+++ dsa_java/arrays/dynamic.java
@@ -1,12 +1,31 @@
-package arrays;
 //staic- fixed size, compile time, stack
 //dynamic- not fixed, run time, heap
+package arrays;
+
+import java.util.*;
+
+
 public class dynamic{
+
     public static void main (String args[]){
-        int [] arr = new int [3];
-        arr[0]=22;
-        arr[1]=23;
-        arr[2]=24;
-        System.out.println(arr[0]);
+     /* int[] arr = new int[3];
+     arr [0]= 30;
+     arr [1]= 40;
+     arr [2]= 50;
+     for (int i = 0; i < arr.length; i++)
+     {
+        System.out.println(arr[i]);
+     } */
+    Scanner sc = new Scanner(System.in);
+    int size = sc.nextInt ();
+    int [] arr = new int[size];
+
+    for (int i=0; i < size; i++){
+    arr[i]=sc.nextInt();
+    }
+
+    for (int i = 0; i < size; i++){
+       System.out.println(arr[i]);
+      }
     }
 }
