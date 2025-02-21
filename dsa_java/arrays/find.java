e461f85fdfc29ba64bee14c8db8871c522c10ad3
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-02-22 01:31:00 +0530

    Add files via upload

---
+++ dsa_java/arrays/find.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class find {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int size = sc.nextInt();
+        int arr [] = new int [size];
+       //input 
+        for (int i = 0; i < size; i++) {
+            arr [i] = sc.nextInt(); 
+        }
+        int x = sc.nextInt();
+        //output
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                System.out.println("x found at index :" + i);
+            }
+        }
+    }
+}
