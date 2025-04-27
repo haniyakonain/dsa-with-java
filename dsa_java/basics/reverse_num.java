de928cdf0be7b5fb13433cf705b0721e76425e6b
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-04-28 04:08:57 +0530

    Update reverse_num.java

--- dsa_java/basics/reverse_num.java
+++ dsa_java/basics/reverse_num.java
@@ -1,12 +1,21 @@
 package basics;
 public class reverse_num {
     public static void main (String [] args) {
-     int num = 12345;
+     /*int num = 12345;
      while (num > 0) {
         int lastdigit = num % 10; // so we get last digit
         System.out.print(lastdigit);
         num /= 10; // so we can remove it
      }
-     System.out.println();
+     System.out.println(); */
+    // to save and print original number 
+    int num = 56789;
+    int rev = 0;
+    while (num > 0) {
+    int lastdigit = num % 10;
+    rev = ( rev * 10 ) + lastdigit;
+    num /= 10;
+    System.out.println(rev);
+    }
     }
 }
