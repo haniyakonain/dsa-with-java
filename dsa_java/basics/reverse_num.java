4d6bd40683bfa88c6642966212478362f0890a71
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-04-28 03:41:00 +0530

    Update reverse_num.java

--- dsa_java/basics/reverse_num.java
+++ dsa_java/basics/reverse_num.java
@@ -3,9 +3,9 @@ public class reverse_num {
     public static void main (String [] args) {
      int num = 12345;
      while (num > 0) {
-        int lastdigit = num % 10;
+        int lastdigit = num % 10; // so we get last digit
         System.out.print(lastdigit);
-        num /= 10;
+        num /= 10; // so we can remove it
      }
      System.out.println();
     }
