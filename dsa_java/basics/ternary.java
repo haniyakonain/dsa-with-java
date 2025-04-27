d50f8075dfa181726fadfc31efdc6c459966f790
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-04-28 01:51:11 +0530

    Add files via upload

---
+++ dsa_java/basics/ternary.java
@@ -0,0 +1,13 @@
+package basics;
+
+import java.util.*;
+public class ternary {
+    public static void main (String args []) {
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       /*ternary operator - 3 operands
+        type variable name = condition? stm1(true) : stm2(false); */
+   String var = (x % 2 == 0) ? "Even" : "Odd";
+    System.out.println(var);
+    }
+}
