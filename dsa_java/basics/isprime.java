d9bc08ad0eea59db1c88f63bbf715099b12c8138
Haniya konain <114371721+haniyakonain@users.noreply.github.com>
2025-04-30 02:24:40 +0530

    Add files via upload

---
+++ dsa_java/basics/isprime.java
@@ -0,0 +1,26 @@
+package basics;
+import java.util.*;
+public class isprime {
+public static void main (String args []) {
+   Scanner sc = new Scanner(System.in);
+   System.out.print("enter a number to check if its prime or not : ");
+   int n = sc.nextInt();
+   if (n == 2 ){
+      System.out.println("n is prime");
+   }
+   else {
+      boolean isPrime = true;
+      for ( int i = 2; i <= Math.sqrt(n); i++) {
+         if (n % i == 0) {
+            isPrime = false;
+         }
+      }
+   if (isPrime == true){
+      System.out.println("n is prime");
+   }
+   else {
+      System.out.println("n is not prime");
+   }
+   }
+ }
+}
