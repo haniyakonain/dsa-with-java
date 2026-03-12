58ffda4a7476f524002eaf2454a64ea646c12937
Haniya konain <haniyakonain94@gmail.com>
2026-03-12 23:27:04 +0530

    Add files via upload

---
+++ dsa_java/bitmanipulation/basics.java
@@ -0,0 +1,9 @@
+public class basics {
+    public static void main(String args []) {
+        int a = 5, b = 6;
+        System.out.println(a & b); //4
+        System.out.println(a | b); // 7
+        System.out.println(a ^ b); // 3
+        System.out.println(~a); // -6
+    }
+}
